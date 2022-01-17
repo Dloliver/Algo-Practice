15b3dbdcf6a2503443f903ba72b15f1afdf2e89d
dennisloliver <doliver9110@gmail.com>
2022-01-17 12:20:21 -0500

    editing the comments

--- Squared.js
+++ Squared.js
@@ -14,7 +14,33 @@ function squared(arr1, arr2) {
   return true;
 }
 
+// Frequency COunters
+
+function squared2(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+  let freqCounter1 = {}
+  let freqCounter2 = {}
+
+  for (let num of arr1) {
+    freqCounter1[num] = freqCounter1[num] + 1 || 1;
+  }
+
+  for (let num of arr2) {
+    freqCounter2[num] = freqCounter2[num] + 1 || 1;
+  }
+
+  for (let key in freqCounter1) {
+    if (!(key ** 2 in freqCounter2)) return false;
+
+    if (freqCounter1[key] !== freqCounter2[key ** 2]) return false;
+  }
+  return true;
+}
+
+
+
+
 console.log(squared([1, 2, 3], [9, 1, 4]));
 console.log(squared([1, 2, 3], [1, 4]));
 console.log(squared([2, 2, 4], [4, 9, 9]));
-console.log(squared([9, 9, 7], [81, 81, 49]));
+console.log(squared2([9, 9, 7], [81, 81, 49]));
